b3dc0409aee341662fd95dc0e9df5925b43a279b
sypark <sangyune.park@k-futuretech.com>
2024-12-10 16:22:29 +0900

    퀴즈 완료 조건 추가 및 선택지 랜덤 구성

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-
 import QUESTION from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTION.length;
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevAnswer) => {
@@ -13,12 +14,24 @@ const Quiz = () => {
     });
   };
 
+  if (quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt='quiz complete' />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id='quiz'>
       <div id='question'>
         <h2>{QUESTION[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-          {QUESTION[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className='answer'>
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
